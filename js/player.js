f561009b7b9bc17788a3e2005279d024a2110031
Auditi <auditibarua6@gmail.com>
2022-08-23 15:29:38 +0300

    selection added

--- js/player.js
+++ js/player.js
@@ -1,51 +1,41 @@
-
+//selection
    function selectLi1(){
-  //  let button  = document.querySelector('.btn-selected');
-   // button.disabled = ('when 5 players added')
-     // if(document.querySelector('btn-selected').value){
-       // button.disabled = true;
-      //}
-      //else{
-        //alert('only 5 players')
-      //}
-
-  
-   
     const node =document.getElementById('li1');
     node.append(document.createTextNode('Kylin Mbappe'));
-    document.querySelector('ul').append(node);
-    
-    
+    document.querySelector('ul').append(node);    
    }
-
-
    function selectLi2(){
-   
     const node = document.getElementById('li2');
     node.append(document.createTextNode('Neymar'));
     document.querySelector('ul').append(node);
     
    }
    function selectLi3(){
-   
     const node = document.getElementById('li3');
     node.append(document.createTextNode('Lionel Messi'));
     document.querySelector('ul').append(node);
    }
    function selectLi4(){
-   
     const node = document.getElementById('li4');
     node.append(document.createTextNode('Hal Robson'));
-   
     document.querySelector('ul').append(node);
    }
    function selectLi5(){
-   
     const node = document.getElementById('li5');
     node.append(document.createTextNode('Javier Pastore'));
     document.querySelector('ul').append(node);
    }
-
+/*
+   function button(){
+      let button  = document.querySelector('.btn-selected');
+    button.disabled = ('when 5 players added')
+      if(document.querySelector('btn-selected').value){
+        button.disabled = true;
+      }
+      else{
+        alert('only 5 players')
+      }
+    }*/
 //calculatlion
 document.getElementById('btn_calculate').addEventListener('click', function(){
   const PerPlayerCost = parseInt(document.getElementById('player_cost').value);
@@ -55,4 +45,13 @@ document.getElementById('btn_calculate').addEventListener('click', function(){
     alert("enter a number")
   }
   expenses_cost.innerText= perPlayer_cost_total;
+});
+
+document.getElementById('btn_calculate_total').addEventListener('click',function(){
+  //const expenses_cost = document.getElementById('expenses_cost').value;
+  const manager_cost = parseInt(document.getElementById('manager_cost').value);
+  const coach_cost = parseInt(document.getElementById('coach_cost').value);
+  const total_cost = document.getElementById('total_cost');
+  const total_cost_calculate  = expenses_cost.value + manager_cost + coach_cost;
+  total_cost.innerText = total_cost_calculate;
 });
